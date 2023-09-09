8aa8840d7b5bac99c8fbcbdf0cf70d3f43b5ee9c
iBrandMedia <alkeneyelesegun@gmail.com>
2023-09-09 06:58:02 -0700

    101-qoute.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - this prints exactly the input on it
+ * Return: always 1
+ */
+int main(void)
+
+{
+
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19n\n", stdout);
+	return (1);
+}
