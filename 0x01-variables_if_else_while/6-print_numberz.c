fc52f3649192c356c5beca909b47048b7caa6bab
iBrandMedia <alkeneyelesegun@gmail.com>
2023-09-12 09:16:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int C;
+
+	C = 0;
+	while
+		(C < 10) {
+			putchar(C + '0');
+			C++;
+		}
+	putchar('\n');
+	return (0);
+}
