c4cbb0e323e989c213cdc623c12a9386a26e8cb6
iBrandMedia <alkeneyelesegun@gmail.com>
2023-09-12 09:36:43 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Prog Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char C;
+
+	C = 'Z';
+	while
+		(C >= 'a') {
+			putchar(C);
+			C--;
+		}
+	putchar('\n');
+	return (0);
+}
+
